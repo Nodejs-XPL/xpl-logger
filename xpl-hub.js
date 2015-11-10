a4d78b60e0c250df6537bd9f4783bbee653077f3
oeuillot <olivier.oeuillot@gmail.com>
2015-11-10 17:27:15 +0100

    Fix code

--- xpl-hub.js
+++ xpl-hub.js
@@ -10,6 +10,24 @@ Xpl.fillCommander(commander);
 
 commander.parse(process.argv);
 
+configuration.hubSupport = true;
+
+var xpl = new Xpl(commander);
+
+xpl.on("error", function(error) {
+  console.log("XPL error", error);
+});
+
+xpl.bind(function(error) {
+  if (error) {
+    console.error("Can not open xpl bridge ", error);
+    process.exit(2);
+    return;
+  }
+
+  console.log("Xpl-hub bind succeed ");
+});
+
 if (commander.headDump) {
   var heapdump = require("heapdump");
   console.log("***** HEAPDUMP enabled **************");
